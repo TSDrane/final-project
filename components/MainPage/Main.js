61415e0f573bf903b2729d442d2779e36a540919
TSDrane <160474643+TSDrane@users.noreply.github.com>
2024-04-04 20:06:36 +1100

    name changes for testing

--- components/MainPage/Main.js
+++ components/MainPage/Main.js
@@ -2,10 +2,11 @@ import Btn from "../Btn/Btn";
 export default function Page() {
     return (
         <div>
-            <h1>Heading</h1>
-            <Btn text="button 1" />
-            <Btn text="button 2" />
-            <Btn text="button 3" />
+            <h1>Feed Me!</h1>
+            <Btn text="Breakfast" />
+            <Btn text="Lunch/Dinner (Non Veg)" />
+            <Btn text="Lunch/Dinner (Veg)" />
+            <Btn text="Desserts" />
         </div>
     
     );

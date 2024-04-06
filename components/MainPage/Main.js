d1b5d4b1ecb9b1236015dcff050151a616fdc02a
TSDrane <160474643+TSDrane@users.noreply.github.com>
2024-04-06 12:28:27 +1100

    import useEffect and useState, async function added to fetch and display recipe title info - mostly for testing... WIP

--- components/MainPage/Main.js
+++ components/MainPage/Main.js
@@ -1,6 +1,25 @@
 import Btn from "../Btn/Btn";
-export default function Page() {
+import { useEffect, useState } from "react";
+
+const MainPage = ()=> {
+    
+    const [recipeData, setRecipeData] = useState(null);
+
+    useEffect (() => {
+        
+        const fetchRecipe = async () => {
+            const url = 'https://www.themealdb.com/api/json/v1/1/random.php'
+            const response = await fetch(url);
+            const data = await response.json();
+            setRecipeData(data.meals[0])
+        };
+        
+        fetchRecipe();
+    },[]);
+
+
     return (
+        <>
         <div>
             <h1>Feed Me!</h1>
             <Btn text="Breakfast" />
@@ -8,6 +27,11 @@ export default function Page() {
             <Btn text="Lunch/Dinner (Veg)" />
             <Btn text="Desserts" />
         </div>
-    
+        <div>
+            {recipeData && <h1>{recipeData.strMeal}</h1>}
+        </div>
+        </>    
     );
 };
+
+export { MainPage }

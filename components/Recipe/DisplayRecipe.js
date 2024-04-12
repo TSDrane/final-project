2ec95af1060be22ac3e6030af6c2c6852c5163c3
TSDrane <160474643+TSDrane@users.noreply.github.com>
2024-04-12 23:26:02 +1000

    blueprint code for props to be passed to with additional functions to clearly show ingredients + instructions

---
+++ components/Recipe/DisplayRecipe.js
@@ -0,0 +1,41 @@
+const DisplayRecipe = ({ recipe }) => {
+
+    const renderIngredients = () => {
+        const ingredients = [];
+        for (let i=1; i <= 20; i++){
+            const ingredient = recipe[`strIngredient${i}`];
+            const measurement = recipe[`strMeasure${i}`];
+            if (ingredient && measurement) {
+                ingredients.push(
+                    <li key={i}>
+                        {ingredient} - {measurement}
+                    </li>
+                );
+            }
+        }
+        return ingredients;
+    };
+
+    const renderInstructions = () => {
+        if (!recipe) return null;
+        return recipe.strInstructions.split("\n").map((line, index) => (
+                <p key={index}>{line}</p>
+        ));
+    };
+
+    return (
+        <div>
+            <h2>{recipe.strMeal}</h2>
+            <h4>{recipe.strArea}</h4>
+            <img src={recipe.strMealThumb} alt={recipe.strMeal} />
+            <h3>Ingredients</h3>
+            <ul>
+                {renderIngredients()}
+            </ul>
+            <h3>Instructions</h3>
+            <p>{renderInstructions()}</p>
+        </div>
+    );
+}
+
+export { DisplayRecipe };

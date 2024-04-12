92fc5c00367eb1c5e706c0fb32bb50eef5802916
TSDrane <160474643+TSDrane@users.noreply.github.com>
2024-04-12 23:29:20 +1000

    main page altered to handle API calls and get random recipe, passing props to DisplayRecipe

--- components/MainPage/Main.js
+++ components/MainPage/Main.js
@@ -1,18 +1,58 @@
-import { DisplayRandomRecipe } from "../Recipe/RandomRecipe";
+import { useState, useEffect } from 'react';
+import { DisplayRecipe } from '../Recipe/DisplayRecipe';
+
 
 const MainPage = ()=> {
-    return (
-        <>
-            <div>
-                <h1>Feed Me!</h1>
-                <button>Breakfast</button>
-                <button>Lunch/Dinner (Non Veg)</button>
-                <button>Lunch/Dinner (Veg)</button>
-                <button>Desserts</button>
-            </div>
-            <DisplayRandomRecipe />
-        </>    
-    );
+    const [recipe, setRecipe] = useState(null);
+
+    useEffect (() => {
+        
+        const loadRecipe = async () => {
+        try {
+            const url = `https://www.themealdb.com/api/json/v1/1/random.php`
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            setRecipe(data.meals[0])
+        } catch (error) {
+            console.log("Failed to fetch recipe: ", error); 
+        }
+    };
+    loadRecipe();
+
+    }, []);
+
+const fetchRandomRecipe = async () => {
+    
+    const responseBeef = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef');
+    const dataBeef = await responseBeef.json();
+
+    const responseChicken = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken');
+    const dataChicken = await responseChicken.json();
+
+    const recipes = [
+        ...dataBeef.meals,
+        ...dataChicken.meals
+    ];
+
+    const randomIndex = Math.floor(Math.random() * recipes.length);
+    const randomRecipeId = recipes[randomIndex].idMeal;
+
+    const recipeResponse = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${randomRecipeId}`);
+    const recipeData = await recipeResponse.json();
+
+    setRecipe(recipeData.meals[0]);
 };
 
-export { MainPage }
+return (
+    <div>
+    <h1>Random Global Recipe!</h1>
+    <button onClick={fetchRandomRecipe}>Get Main Meals</button>
+    {recipe && <DisplayRecipe recipe={recipe} />}
+    </div>
+);
+}
+
+export { MainPage };

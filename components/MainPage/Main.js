354be654269e56c1a70117d69121673f0419d3b1
TSDrane <160474643+TSDrane@users.noreply.github.com>
2024-04-12 22:03:21 +1000

    renamed in place of button component no longer existing

--- components/MainPage/Main.js
+++ components/MainPage/Main.js
@@ -1,4 +1,3 @@
-import { Button } from "../Btn/Btn";
 import { DisplayRandomRecipe } from "../Recipe/RandomRecipe";
 
 const MainPage = ()=> {
@@ -6,10 +5,10 @@ const MainPage = ()=> {
         <>
             <div>
                 <h1>Feed Me!</h1>
-                <Button text="Breakfast" />
-                <Button text="Lunch/Dinner (Non Veg)" />
-                <Button text="Lunch/Dinner (Veg)" />
-                <Button text="Desserts" />
+                <button>Breakfast</button>
+                <button>Lunch/Dinner (Non Veg)</button>
+                <button>Lunch/Dinner (Veg)</button>
+                <button>Desserts</button>
             </div>
             <DisplayRandomRecipe />
         </>    
